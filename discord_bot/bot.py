8bea9af1b6de49e3e12d08ef86af8bff751ca9aa
AJ Stein <as169117@ohio.edu>
2021-11-20 23:00:14 -0500

    got !game working

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -8,30 +8,76 @@ import os # for the token
 import discord # pip install -U discord.py
 from discord.ext import commands
 from dotenv import load_dotenv # $ pip install -U python-dotenv
+import random
 import cmd
 
-# making an instance of the bot
-client = discord.Client()
+# making an instance of the bot and setting prefix
+client = commands.Bot(command_prefix='!')
+client.remove_command('help')
 
 # telling us the bot is online
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-# end of function
+  cmd.create_connection('game.db')
+# end of function  
+
+# help command
+@client.command(pass_context=True)
+async def help(ctx):
+  # color of the left bar
+  embed = discord.Embed(
+    colour = discord.Colour.dark_teal()
+  )
+  # contents of the embed
+  embed.set_author(name='Commands:')
+  embed.add_field(name='Command: !game \'[game name]\' [optional console]', value='Returns: title, console, genre, publisher\nMore Info: Without specifying a console itll return the first one it sees. Game must be wrapped with \'\' and lowercase. This command is for search specific games that you know is formatted correctly, if you are unsure of the format of the game name or would rather have partial matches see !matchsearchgame.', inline=False)
+  
+  # send a message about cmds
+  await ctx.send(embed=embed)
+# end of help
+
+########################################################################################################
+# Commands
+# !game [game_name][opt console] -> details about the game, like: title, console, genre, publisher
+@client.command()
+async def game(ctx, *args):
+  # check if we got args
+  if args:
+    output = cmd.get_game(args)
+    # if len(output) == 0:
+    #       await ctx.send("")
+    # await ctx.send("Title: "+output[0]+", Console: "+output[1]+", Genre: "+output[2]+", Publisher: "+output[3])
+  else:
+    await ctx.send("Missing game name")
+# end of game cmd
 
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('!hi'):
-    await message.channel.send("hi")
-
-
+########################################################################################################
 # get discord token
 load_dotenv()
 client.run(os.getenv('TOKEN'))
+
+# To do list in order:
+# !game [game_name][opt console] -> details about the game, like: title, console, genre, publisher
+# !extendedgame [game_name] -> title, console, genre, publisher, developer, release date, last update
+# !allgames [keyword] -> all game titles that match that keyword
+# !sales [game_name] -> sale info of the game
+# !coverart [game] -> coverart of the game
+# !moreinfo [game] -> link the wiki  of the game
+# !devsearch [keyword] -> games by a dev
+# !publishersearch [keyword] -> games by the publisher
+# !matchsearchgame [game] -> match part of the game name to games
+# !matchsearchpublisher
+# !matchsearchdeveloper
+# !releasedatesearch [date1][date2] -> games between those dates
+# !lastupdatedatesearch [date1][date2] -> games between those dates
+# !topscore [optional num] -> games above this number, otherwise just the top games
+# !bottomscore [opt num] -> bottom games below this num or just bottom gamees
+# !salessearch [game][optional region] -> give total number unless opt region isnt null
+# graph of top sold games by publisher
+# graph of top sold games by console
+# graph of top sold games by genre
 
 # Ideas for the bot
 # get score on game (top/bottom X amount)
--- discord_bot/cmd.py
+++ discord_bot/cmd.py
@@ -7,25 +7,85 @@
 import sqlite3
 from sqlite3 import Error
 import math
-
-def main():
-    # This is the connection to the db
-    global dbconnection
-    dbconnection = create_connection('game.db')
-# end of main
-
+import os.path
+########################################################################################################
 # connect to the db
 def create_connection(db_file):
-    conn = None
+    global dbcon
+    dbcon = None
     try:
-        conn = sqlite3.connect(db_file)
-        return conn
+        # something with pathing to fix getting a db connection found from stackoverflow
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        dbpath = os.path.join(BASE_DIR, "game.db")
+        dbcon = sqlite3.connect(dbpath)
+        
+        # return dbcon
     except Error as e:
         print(e)
-
-    return conn
+    
 # end of fun
+########################################################################################################
+# Commands
+def get_game(args):
+    # get the game name or console
+    game = ''
+    console = ''
+    
+    # getting the start and end
+    start, end = False, False
+    
+    # counter for getting the console if given
+    counter = 0
+    
+    # parse the args
+    if len(args) == 1:
+        game = args[0]
+    else:
+        for i in range(len(args)):
+            # never got a ' 
+            if args[0].find('\'') == -1:
+                return -1
+            else:
+                # find where it ends and add it to game
+                if start == False: # get the slice at the start so we dont get a '
+                    # game += args[0][1:]
+                    game += args[0]
+                    game += ' ' 
+                    start = True
+                    counter += 1
+                elif start == True and args[i].find('\'') != -1: # found the end
+                    # game += args[i][:-1]
+                    game += args[i]
+                    counter += 1
+                    end = True
+                elif start == True and args[i].find('\'') == -1 and end == False: # middle
+                    game += args[i]
+                    game += ' ' # need a space
+                    counter += 1
+    # end of else
+    # check for console
+    if counter < len(args):
+        console = args[len(args) - 1]
 
-# declaring main
-if __name__ == "__main__":
-    main()
+    console = '\'' + console + '\''
+    print(game, "a")
+
+    # get database cur
+    cur = dbcon.cursor()
+    
+    # do the sql query
+    if len(console) == 0:
+        game = game[1:-1]
+        cur.execute("""SELECT title, console, genre, publisher FROM games WHERE title='%s'""" % (game,) )
+    else:
+        # we got a console
+        cur.execute("""SELECT title, console, genre, publisher FROM games WHERE title=%s AND console=%s""" % (game, console) )
+    
+    # get the query results
+    retstring = cur.fetchone()
+    print(retstring)
+    # close cur
+    cur.close()
+    # return string
+    return retstring
+# end of function
